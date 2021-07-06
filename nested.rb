1e4492ec78e53501b708a150e4b0849a584f97aa
Eric Nganga <ericmainaedits@gmail.com>
2021-07-05 21:55:58 -0500

    completed assignment

--- nested.rb
+++ nested.rb
@@ -15,11 +15,13 @@ def hopper
         :languages => ["C"]
       }
     }
+    programmer_hash[:grace_hopper]
 
 end
 
 def alan_kay_is_known_for
 	# What combination of keys would you use to return the value of the :known_for key of :alan_kay?
+
 	
 	programmer_hash = 
  		{
@@ -36,6 +38,7 @@ def alan_kay_is_known_for
         :languages => ["C"]
       }
     }
+    programmer_hash[:alan_kay][:known_for]
 
 end
 
@@ -55,6 +58,8 @@ def dennis_ritchies_language
         :languages => ["C"]
       }
     }
+
+	programmer_hash[:dennis_ritchie][:languages][0]
 
 end
 
@@ -82,6 +87,11 @@ def adding_matz
       }
     }
 
+    programmer_hash[:yukihiro_matsumoto] = {}
+		 programmer_hash[:yukihiro_matsumoto][:known_for] = "Ruby"
+		 programmer_hash[:yukihiro_matsumoto][:languages] = ["LISP", "C"]
+		 programmer_hash
+
 end
 
 def changing_alan
@@ -104,6 +114,11 @@ def changing_alan
       }
     }
 
+    alans_new_info = "GUI"
+
+	programmer_hash[:alan_kay][:known_for] = alans_new_info
+	programmer_hash
+
 end
 
 def adding_to_dennis
@@ -125,5 +140,7 @@ def adding_to_dennis
         :languages => ["C"]
       }
     }
+    programmer_hash[:dennis_ritchie][:languages] << "Assembly"
+	programmer_hash
 
 end
